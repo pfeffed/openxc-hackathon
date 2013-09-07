8d47d85c4b1fc449169056c1e3eb02bcb130d6c2
rpatwa <ruchir.patwa90@gmail.com>
2013-09-07 11:53:02 -0700

    test service

--- OpenXCHackathon/src/OpenXCHackathonService.java
+++ OpenXCHackathon/src/OpenXCHackathonService.java
@@ -1,10 +1,19 @@
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 
 public class OpenXCHackathonService extends Service{
 
+		String tag="TestService";
+	   @Override
+	   public void onCreate() {
+	       super.onCreate();
+	       Toast.makeText(this, "Service created...", Toast.LENGTH_LONG).show();      
+	       Log.i(tag, "Service created...");
+	   }
 	  @Override
 	  public int onStartCommand(Intent intent, int flags, int startId) {
 	    //TODO do something useful
