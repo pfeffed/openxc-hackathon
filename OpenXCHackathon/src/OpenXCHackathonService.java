28ee41dc093c166144ac4035406e8b27dae6d732
rpatwa <ruchir.patwa90@gmail.com>
2013-09-07 11:45:59 -0700

    changed activity to service

---
+++ OpenXCHackathon/src/OpenXCHackathonService.java
@@ -0,0 +1,19 @@
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class OpenXCHackathonService extends Service{
+
+	  @Override
+	  public int onStartCommand(Intent intent, int flags, int startId) {
+	    //TODO do something useful
+	    return Service.START_NOT_STICKY;
+	  }
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
